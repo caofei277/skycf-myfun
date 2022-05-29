7cdba64e047334ebde679e583ae3cac6a6fdfb8d
曹飞 <32540877@qq.com>
2022-05-29 14:10:31 +0800

    localstorage 获取方法

--- lib/index.ts
+++ lib/index.ts
@@ -24,3 +24,32 @@ export function mySetStorage(key: string, value: string, expired: number = 0, is
   }
   return value;
 }
+
+
+
+/*
+   * localstorage 获取方法
+   * @ param {String} 	key 键
+   * @ param {String} 	expired 存储时为非必须字段，所以有可能取不到，默认为 Date.now+1
+   */
+export function myGetStorage(key: string)  {
+  const now = Date.now() / 1000;
+  const expired = Number(localStorage.getItem(`${key}__expires__`)) || now + 1;
+
+  if (now >= expired) {
+    localStorage.removeItem(key);
+    return null;
+  } else {
+    // console.log('还没有过期' + now + '|' + expired)
+  }
+
+
+  const val = localStorage.getItem(key);
+
+  try {
+    return typeof(val) === "string" && val ? JSON.parse(val) : null;
+  } catch (e) {
+    return val;
+  }
+
+}
