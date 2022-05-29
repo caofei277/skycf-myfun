4a19d6eb8a4858f07e4e5813faa269f6d7912745
曹飞 <32540877@qq.com>
2022-05-29 23:10:09 +0800

    修复一些问题

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -36,3 +36,11 @@ export declare function mytest(key: string): string;
  * @param isFullExpired  isFullExpired 过期时间是否为完整过期时间 如果不是 则需要以当前时间加过期时间  如果是完整时间 则说明是完整到期时间戳 则直接使用即可
  */
 export declare function mySetStorage234(key: string, value: string, expired?: number, isFullExpired?: boolean): string;
+/**
+ * localstorage 存储方法(可设置有效期)
+ * @param key key 键
+ * @param value value 值，
+ * @param expired expired 过期时间，以秒为单位，非必须
+ * @param isFullExpired  isFullExpired 过期时间是否为完整过期时间 如果不是 则需要以当前时间加过期时间  如果是完整时间 则说明是完整到期时间戳 则直接使用即可
+ */
+export declare function mySetStorage456(key: string, value: string, expired?: number, isFullExpired?: boolean): string;
