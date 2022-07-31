a4c838a22d6e1fbca6d9f798f07c20e4c1bea933
曹飞 <32540877@qq.com>
2022-07-31 14:33:38 +0800

    新增精确加法方法

--- lib/index.ts
+++ lib/index.ts
@@ -803,3 +803,16 @@ export function myCurrencyToCapital(money: any) {
   }
   return chineseStr;
 }
+
+
+/**
+ * 精确加法
+ * @param num1
+ * @param num2
+ */
+export function myMathAdd(num1: number, num2: number) {
+  const num1Digits = (num1.toString().split(".")[1] || "").length;
+  const num2Digits = (num2.toString().split(".")[1] || "").length;
+  const baseNum = Math.pow(20,Math.max(num1Digits,num2Digits));
+  return (num1 * baseNum + num2 * baseNum) / baseNum;
+}
